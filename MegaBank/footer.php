d1ab402ad4e0439ba17d287daa2e2640d1a43c52
yashbharne <88792908+yashbharne@users.noreply.github.com>
2021-08-19 13:46:18 +0530

    Add files via upload

---
+++ MegaBank/footer.php
@@ -0,0 +1,4 @@
+<div id="container">
+    <div id="header"> Yash Bharne <br>
+      Project of the Sparks Foundation</div>
+ </div>
---
+++ MegaBank/index.php
@@ -0,0 +1,20 @@
+<?php include('nav.php') ?>
+
+
+  <h1> Mega Bank</h1>
+
+
+  <div class="bg">
+    <div class="Center">
+      <ul>
+        <li class="operations"><a href="user.php"><button class="btn" id="red"> Users</button></a></li>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="red">Trasnfer Money </button></a></li>
+        <li class="operations"><a href="transactionhistory.php"><button class="btn" id="red"> Transaction History</button></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
